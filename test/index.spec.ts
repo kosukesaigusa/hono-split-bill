35e7b355107fd735a84b51ace9cb94ddd39cdb6c
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-04-06 09:41:52 +0900

    test: update test mock

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -18,8 +18,8 @@ describe('GET /', () => {
 
 describe('GET /api/groups/:groupUuid', () => {
   test('should return status 200', async () => {
-    vi.mock('../src/models/use-cases/fetch-group', () => ({
-      fetchGroup: vi.fn().mockResolvedValue({
+    vi.mock('../src/models/repositories/group', () => ({
+      queryGroup: vi.fn().mockResolvedValue({
         group_id: 1,
         group_uuid: '123e4567-e89b-12d3-a456-426614174000',
         group_name: 'Test Group',
@@ -42,8 +42,8 @@ describe('GET /api/groups/:groupUuid', () => {
 
 describe('GET /api/groups/:groupUuid/members', () => {
   test('should return status 200', async () => {
-    vi.mock('../src/models/use-cases/fetch-group-members', () => ({
-      fetchGroupMembers: vi.fn().mockResolvedValue([
+    vi.mock('../src/models/repositories/group-members', () => ({
+      queryGroupMembers: vi.fn().mockResolvedValue([
         {
           member_id: 1,
           member_name: 'Test User',
@@ -70,23 +70,27 @@ describe('GET /api/groups/:groupUuid/members', () => {
 
 describe('GET /api/groups/:groupUuid/expenses', () => {
   test('should return status 200', async () => {
-    vi.mock('../src/models/use-cases/fetch-group-expenses', () => ({
-      fetchGroupExpenses: vi.fn().mockResolvedValue([
+    vi.mock('../src/models/repositories/group-expenses', () => ({
+      queryGroupExpenses: vi.fn().mockResolvedValue([
         {
           expense_id: 1,
           amount: 100,
           description: 'Test Expense',
           created_at: '2021-01-01T00:00:00Z',
-          paid_by_member: {
-            member_id: 1,
-            member_name: 'Test User',
-          },
-          participant_members: [
-            {
-              member_id: 1,
-              member_name: 'Test User',
-            },
-          ],
+          paid_by_member_id: 1,
+          paid_by_member_name: 'Test User 1',
+          participant_member_id: 1,
+          participant_member_name: 'Test User 1',
+        },
+        {
+          expense_id: 1,
+          amount: 100,
+          description: 'Test Expense',
+          created_at: '2021-01-01T00:00:00Z',
+          paid_by_member_id: 2,
+          paid_by_member_name: 'Test User 1',
+          participant_member_id: 2,
+          participant_member_name: 'Test User 2',
         },
       ]),
     }))
@@ -107,12 +111,16 @@ describe('GET /api/groups/:groupUuid/expenses', () => {
           created_at: '2021-01-01T00:00:00Z',
           paid_by_member: {
             member_id: 1,
-            member_name: 'Test User',
+            member_name: 'Test User 1',
           },
           participant_members: [
             {
               member_id: 1,
-              member_name: 'Test User',
+              member_name: 'Test User 1',
+            },
+            {
+              member_id: 2,
+              member_name: 'Test User 2',
             },
           ],
         },
