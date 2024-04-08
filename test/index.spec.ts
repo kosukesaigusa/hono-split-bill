1681550c58b7c0d78cf94455458c9638ea434d9d
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-04-08 10:58:16 +0900

    test: add tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,4 +1,8 @@
 import app from '../src'
+import { diContainer } from '../src/di/di-config'
+import { IGroupRepository } from '../src/models/repositories/group'
+import { IGroupExpensesRepository } from '../src/models/repositories/group-expenses'
+import { IGroupMembersRepository } from '../src/models/repositories/group-members'
 
 const MOCK_ENV = {
   DB: {
@@ -10,21 +14,42 @@ const MOCK_ENV = {
 
 describe('GET /', () => {
   test('should return status 200', async () => {
-    const res = await app.request('/')
+    const res = await app.request('/', {}, MOCK_ENV)
     expect(res.status).toBe(200)
     expect(await res.json()).toEqual({ message: 'Hello, World!' })
   })
 })
 
 describe('GET /api/groups/:groupUuid', () => {
+  test('should return status 404 if group query result is undefined', async () => {
+    class MockUndefinedGroupRepository implements IGroupRepository {
+      async fetchGroup() {
+        return undefined
+      }
+    }
+
+    diContainer.override('GroupRepository', new MockUndefinedGroupRepository())
+
+    const res = await app.request(
+      '/api/groups/123e4567-e89b-12d3-a456-426614174000',
+      {},
+      MOCK_ENV
+    )
+    expect(res.status).toBe(404)
+  })
+
   test('should return status 200', async () => {
-    vi.mock('../src/models/repositories/group', () => ({
-      queryGroup: vi.fn().mockResolvedValue({
-        group_id: 1,
-        group_uuid: '123e4567-e89b-12d3-a456-426614174000',
-        group_name: 'Test Group',
-      }),
-    }))
+    class MockGroupRepository implements IGroupRepository {
+      async fetchGroup() {
+        return {
+          group_id: 1,
+          group_uuid: '123e4567-e89b-12d3-a456-426614174000',
+          group_name: 'Test Group',
+        }
+      }
+    }
+
+    diContainer.override('GroupRepository', new MockGroupRepository())
 
     const res = await app.request(
       '/api/groups/123e4567-e89b-12d3-a456-426614174000',
@@ -42,14 +67,21 @@ describe('GET /api/groups/:groupUuid', () => {
 
 describe('GET /api/groups/:groupUuid/members', () => {
   test('should return status 200', async () => {
-    vi.mock('../src/models/repositories/group-members', () => ({
-      queryGroupMembers: vi.fn().mockResolvedValue([
-        {
-          member_id: 1,
-          member_name: 'Test User',
-        },
-      ]),
-    }))
+    class MockGroupMembersRepository implements IGroupMembersRepository {
+      async fetchGroupMembers() {
+        return [
+          {
+            member_id: 1,
+            member_name: 'Test User',
+          },
+        ]
+      }
+    }
+
+    diContainer.override(
+      'GroupMembersRepository',
+      new MockGroupMembersRepository()
+    )
 
     const res = await app.request(
       '/api/groups/123e4567-e89b-12d3-a456-426614174000/members',
@@ -70,30 +102,37 @@ describe('GET /api/groups/:groupUuid/members', () => {
 
 describe('GET /api/groups/:groupUuid/expenses', () => {
   test('should return status 200', async () => {
-    vi.mock('../src/models/repositories/group-expenses', () => ({
-      queryGroupExpenses: vi.fn().mockResolvedValue([
-        {
-          expense_id: 1,
-          amount: 100,
-          description: 'Test Expense',
-          created_at: '2021-01-01T00:00:00Z',
-          paid_by_member_id: 1,
-          paid_by_member_name: 'Test User 1',
-          participant_member_id: 1,
-          participant_member_name: 'Test User 1',
-        },
-        {
-          expense_id: 1,
-          amount: 100,
-          description: 'Test Expense',
-          created_at: '2021-01-01T00:00:00Z',
-          paid_by_member_id: 2,
-          paid_by_member_name: 'Test User 1',
-          participant_member_id: 2,
-          participant_member_name: 'Test User 2',
-        },
-      ]),
-    }))
+    class MockGroupExpensesRepository implements IGroupExpensesRepository {
+      async fetchGroupExpenses() {
+        return [
+          {
+            expense_id: 1,
+            amount: 100,
+            description: 'Test Expense',
+            created_at: '2021-01-01T00:00:00Z',
+            paid_by_member_id: 1,
+            paid_by_member_name: 'Test User 1',
+            participant_member_id: 1,
+            participant_member_name: 'Test User 1',
+          },
+          {
+            expense_id: 1,
+            amount: 100,
+            description: 'Test Expense',
+            created_at: '2021-01-01T00:00:00Z',
+            paid_by_member_id: 2,
+            paid_by_member_name: 'Test User 1',
+            participant_member_id: 2,
+            participant_member_name: 'Test User 2',
+          },
+        ]
+      }
+    }
+
+    diContainer.override(
+      'GroupExpensesRepository',
+      new MockGroupExpensesRepository()
+    )
 
     const res = await app.request(
       '/api/groups/123e4567-e89b-12d3-a456-426614174000/expenses',
